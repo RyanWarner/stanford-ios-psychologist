44c7aa3e8858e4d617aff862a2ccca19ad5f3221
Ryan Warner <warner.ryan@gmail.com>
2016-03-07 07:57:35 -0800

    Attempt to set history popover size, but its not quite right

--- Psychologist/Psychologist/TextViewController.swift
+++ Psychologist/Psychologist/TextViewController.swift
@@ -21,5 +21,24 @@ class TextViewController: UIViewController {
 			textView?.text = text
 		}
 	}
+	
+	override var preferredContentSize: CGSize
+	{
+		get
+		{
+			if textView != nil && presentingViewController != nil
+			{
+				return textView.sizeThatFits( presentingViewController!.view.bounds.size)
+			}
+			else
+			{
+				return super.preferredContentSize
+			}
+		}
+		
+		set {
+			super.preferredContentSize = newValue
+		}
+	}
 
 }
